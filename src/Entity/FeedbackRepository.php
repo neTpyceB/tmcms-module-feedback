5082a280a7183d2073716580b0829efd76f1d26c
Vadims <pvman@inbox.ru>
2015-12-06 14:47:31 +0200

    Create FeedbackRepository.php

---
+++ src/Entity/FeedbackRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TMCms\Modules\Feedback\Entity;
+
+use neTpyceB\TMCms\Orm\EntityRepository;
+
+class FeedbackRepository extends EntityRepository {
+    protected $db_table = 'm_feedback';
+}
