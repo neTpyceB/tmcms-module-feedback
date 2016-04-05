5ea9dc7bd0baeb8a9dbfa425e46cc8a0137069a7
neTpyceB <pvman@inbox.ru>
2016-04-05 15:34:32 +0300

    Filter by email and can remove dupes

--- src/CmsFeedback.php
+++ src/CmsFeedback.php
@@ -7,7 +7,10 @@ use TMCms\HTML\Cms\CmsTable;
 use TMCms\HTML\Cms\Column\ColumnAccept;
 use TMCms\HTML\Cms\Column\ColumnData;
 use TMCms\HTML\Cms\Column\ColumnDelete;
+use TMCms\HTML\Cms\Columns;
 use TMCms\HTML\Cms\Element\CmsHtml;
+use TMCms\HTML\Cms\Filter\Text;
+use TMCms\HTML\Cms\FilterForm;
 use TMCms\Strings\Converter;
 use TMCms\Modules\Feedback\Entity\Feedback;
 use TMCms\Modules\Feedback\Entity\FeedbackRepository;
@@ -22,15 +25,19 @@ class CmsFeedback
         $feedback_collection = new FeedbackRepository();
         $feedback_collection->addOrderByField('date_created', true);
 
+        echo Columns::getInstance()
+            ->add('<a href="?p='. P .'&do=_remove_dupes" class="btn btn-warning">Remove duplicate emails</a>', ['align' => 'right'])
+        ;
+
         echo CmsTable::getInstance()
             ->addData($feedback_collection)
             ->addColumn(ColumnData::getInstance('date_created')
                 ->enableOrderableColumn()
-                ->nowrap(true)
-                ->align('right')
-                ->href('?p=' . P . '&do=view&id={%id%}')
+                ->disableNewlines()
+                ->enableRightAlign()
+                ->setHref('?p=' . P . '&do=view&id={%id%}')
                 ->dataType('ts2datetime')
-                ->title('Date')
+                ->setTitle('Date')
             )
             ->addColumn(ColumnData::getInstance('name')
                 ->enableOrderableColumn()
@@ -43,11 +50,18 @@ class CmsFeedback
                 ->dataType('email')
             )
             ->addColumn(ColumnAccept::getInstance('done')
-                ->href('?p=' . P . '&do=_done&id={%id%}')
+                ->setHref('?p=' . P . '&do=_done&id={%id%}')
                 ->enableOrderableColumn()
             )
             ->addColumn(ColumnDelete::getInstance()
-                ->href('?p=' . P . '&do=_delete&id={%id%}')
+                ->setHref('?p=' . P . '&do=_delete&id={%id%}')
+            )
+            ->attachFilterForm(
+                FilterForm::getInstance()
+                    ->setWidth('100%')
+                    ->addFilter('Email', Text::getInstance('email')
+                        ->actAs('like')
+                    )
             );
     }
 
@@ -103,6 +117,22 @@ class CmsFeedback
 
         $feedback = new Feedback($feedback_id);
         $feedback->deleteObject();
+
+        back();
+    }
+
+    public function _remove_dupes() {
+        $all = new FeedbackRepository();
+
+        $used_email = [];
+        foreach ($all->getAsArrayOfObjects() as $feedback) { /** @var $feedback Feedback */
+            if (!in_array($feedback->getEmail(), $used_email)) {
+                $used_email[] = $feedback->getEmail();
+                continue;
+            }
+
+            $feedback->deleteObject();
+        }
 
         back();
     }
